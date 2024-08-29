6e20894989c0cd1e426109ca216260b8e19f8a02
Atakan Bacaksız <32418624+AtakanBacaksiz@users.noreply.github.com>
2024-08-29 13:36:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ $(window).on("load", function () {
 function getModalElements() {
   return {
     cmodal: document.querySelector("#contact-modal"),
+    modalcard: document.querySelector("#modal-card"),
     cmodalcard: document.querySelector("#contact-card"),
     contactBtn: document.querySelector("#contact-btn"),
     contactBtn2: document.querySelector("#contact-btn2"),
+    span: document.querySelector(".modal-background"),
     cspan: document.querySelector(".modal-background.is-contact"),
+    copyk: document.querySelector("#copy-mail-btn"),
     copyc: document.querySelector("#copy-mail-cbtn"),
+    footercopy: document.querySelector("#footer-copy"),
     shortcuthero: document.querySelector("#shortcut-hero"),
     animatedDiv: document.querySelector(".toast-message"),
   };
@@ -114,12 +118,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const {
+  modal,
   cmodal,
+  modalcard,
   cmodalcard,
   contactBtn,
   contactBtn2,
+  span,
   cspan,
+  copyk,
   copyc,
+  footercopy,
   shortcuthero,
   animatedDiv,
 } = getModalElements();
@@ -155,6 +164,18 @@ if (copyc) {
   });
 }
 
+if (copyk) {
+  copyk.addEventListener("click", function () {
+    showToast(animatedDiv);
+  });
+}
+
+if (footercopy) {
+  footercopy.addEventListener("click", function () {
+    showToast(animatedDiv);
+  });
+}
+
 if (contactBtn) {
   contactBtn.addEventListener("click", function () {
     cmodal.style.display = "flex";
@@ -172,16 +193,17 @@ if (contactBtn) {
 
 if (shortcuthero) {
   shortcuthero.addEventListener("click", function () {
-    cmodal.style.display = "flex";
+    modal.style.display = "flex";
+    cmodal.style.display = "none";
 
-    cmodalcard.style.transform = "scale(0.99) translateY(-10px)";
-    cmodalcard.style.opacity = "0";
+    modalcard.style.transform = "scale(0.99) translateY(-10px)";
+    modalcard.style.opacity = "0";
 
     requestAnimationFrame(() => {
-      cmodalcard.style.transition =
+      modalcard.style.transition =
         "transform 0.3s cubic-bezier(0.25, 0.8, 0.25, 1), opacity 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)";
-      cmodalcard.style.transform = "scale(1) translateY(0)";
-      cmodalcard.style.opacity = "1";
+      modalcard.style.transform = "scale(1) translateY(0)";
+      modalcard.style.opacity = "1";
     });
   });
 }
@@ -189,6 +211,7 @@ if (shortcuthero) {
 if (contactBtn2) {
   contactBtn2.addEventListener("click", function () {
     cmodal.style.display = "flex";
+    modal.style.display = "none";
     cmodalcard.style.transform = "scale(0.99) translateY(-10px)";
     cmodalcard.style.opacity = "0";
 
@@ -201,57 +224,109 @@ if (contactBtn2) {
   });
 }
 
-if (
-  event.key === "c" &&
-  !event.metaKey &&
-  !event.ctrlKey &&
-  !event.altKey &&
-  !event.shiftKey
-) {
-  if (cmodalcard && cmodal && modal) {
-    cmodalcard.style.transform = "scale(1) translateY(0)";
-    cmodalcard.animate(
-      [
+document.addEventListener("keydown", function (event) {
+  if ((event.ctrlKey || event.metaKey) && event.key === "k") {
+    if (modalcard && modal) {
+      modalcard.style.transform = "scale(0.99) translateY(-10px)";
+      modalcard.style.opacity = "0";
+      modal.style.display = "flex";
+      cmodal.style.display = "none";
+
+      requestAnimationFrame(() => {
+        modalcard.style.transition =
+          "transform 0.3s cubic-bezier(0.25, 0.8, 0.25, 1), opacity 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)";
+        modalcard.style.transform = "scale(1) translateY(0)";
+        modalcard.style.opacity = "1";
+      });
+
+      event.preventDefault();
+    }
+  }
+
+  if (
+    event.key === "c" &&
+    !event.metaKey &&
+    !event.ctrlKey &&
+    !event.altKey &&
+    !event.shiftKey
+  ) {
+    if (cmodalcard && cmodal && modal) {
+      cmodalcard.style.transform = "scale(1) translateY(0)";
+      cmodalcard.animate(
+        [
+          {
+            transform: "scaleX(0.99) scaleY(0.99) translateY(-10px)",
+            opacity: 0,
+          },
+          { transform: "scale(1) translateY(0)", opacity: 1 },
+        ],
         {
-          transform: "scaleX(0.99) scaleY(0.99) translateY(-10px)",
-          opacity: 0,
-        },
-        { transform: "scale(1) translateY(0)", opacity: 1 },
-      ],
-      {
-        duration: 100,
-        easing: "ease-out",
-        fill: "forwards",
-      }
-    );
-    cmodal.style.display = cmodal.style.display === "flex" ? "none" : "flex";
-    modal.style.display = "none";
-    event.preventDefault();
-  }
-}
-
-if (event.key === "Escape") {
-  if (modalcard && modal) {
-    modalcard.style.transition =
-      "transform 0.2s cubic-bezier(0.25, 0.8, 0.25, 1), opacity 0.2s cubic-bezier(0.25, 0.8, 0.25, 1)";
-    modalcard.style.transform = "scale(0.99) translateY(-10px)";
-    modalcard.style.opacity = "0";
-
-    setTimeout(() => {
+          duration: 100,
+          easing: "ease-out",
+          fill: "forwards",
+        }
+      );
+      cmodal.style.display = cmodal.style.display === "flex" ? "none" : "flex";
       modal.style.display = "none";
-    }, 0);
-
-    event.preventDefault();
+      event.preventDefault();
+    }
   }
 
-  if (cmodal) {
-    cmodal.style.display = "none";
-    event.preventDefault();
+  if (event.key === "Escape") {
+    if (modalcard && modal) {
+      modalcard.style.transition =
+        "transform 0.2s cubic-bezier(0.25, 0.8, 0.25, 1), opacity 0.2s cubic-bezier(0.25, 0.8, 0.25, 1)";
+      modalcard.style.transform = "scale(0.99) translateY(-10px)";
+      modalcard.style.opacity = "0";
+
+      setTimeout(() => {
+        modal.style.display = "none";
+      }, 0);
+
+      event.preventDefault();
+    }
+
+    if (cmodal) {
+      cmodal.style.display = "none";
+      event.preventDefault();
+    }
   }
+
+  if (
+    ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key) &&
+    modal &&
+    modal.style.display === "flex"
+  ) {
+    event.preventDefault();
+    const currentLink = document.activeElement.closest("a:not(#ignoreLink)");
+    const links = modal.querySelectorAll("a:not(#ignoreLink)");
+    const currentIndex = Array.from(links).indexOf(currentLink);
+    const nextIndex =
+      (currentIndex +
+        (event.key === "ArrowUp" || event.key === "ArrowLeft" ? -1 : 1) +
+        links.length) %
+      links.length;
+    links[nextIndex].focus();
+    modal.scrollTop = links[nextIndex].offsetTop - modal.offsetTop;
+  }
+});
+
+if (span) {
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
 }
 
 if (cspan) {
   cspan.onclick = function () {
     cmodal.style.display = "none";
   };
+}
+
+if (navigator.userAgent.indexOf("Mac OS X") !== -1) {
+  $("#ctrl-e, #ctrl-k").hide();
+  $("#cmd-e, #cmd-k").show();
+} else {
+  $("#ctrl-e, #ctrl-k").show();
+  $("#cmd-e, #cmd-k").hide();
 }
