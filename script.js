a5f8c02b01e7e540021cd79d0ee7ef3f0590c4f9
Atakan Bacaksız <32418624+AtakanBacaksiz@users.noreply.github.com>
2025-01-02 20:52:54 +0000

    test

--- script.js
+++ script.js
@@ -376,7 +376,7 @@ $(document).ready(function () {
         if (!closeButton) {
           closeButton = $(
             `<div class="button is-close">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="embed-icon 24">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="embed-icon.24">
                 <path d="M7.75 7.75L16.25 16.25M16.25 7.75L7.75 16.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
               </svg>
             </div>`
